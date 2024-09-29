de83989ae30ef35f9f1e581edcac98c9962ca6a8
Fatima Modupe Bello <mailtomodupe@gmail.com>
2024-09-29 23:36:28 +0100

    video update

--- src/components/expertise.tsx
+++ src/components/expertise.tsx
@@ -17,20 +17,18 @@ const Expertise = () => {
             and organizations. Our expertise encompasses:
           </h4>
 
-    <div className="lg:w-[50%] mx-auto pb-4">
+    <div className="lg:w-[50%] w-full mx-auto pb-4">
    
       {videoError ? (
         <img 
           src={Image.expertise} 
           alt="Fallback image" 
-          width="560" 
-          height="315"
+         
         />
       ) : (
      
         <iframe
-        width="560"
-        height="315"
+      
         src="https://www.youtube.com/embed/5r_7VYk-VGM"
         title="YouTube video player"
         frameBorder="0"
